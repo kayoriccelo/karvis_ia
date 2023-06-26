252f77b3f9d58549bd10690ee94e1762810921f0
Kayo Riccelo <kayo.riccelo@hotmail.com>
2023-06-25 23:07:54 -0300

    - improved app vocabulary (melhorado aplicativo vocabulário).

--- data/vocabulary/models.py
+++ data/vocabulary/models.py
@@ -5,7 +5,7 @@ from .choices import C_TYPE_VOCABULARY
 
 class Vocabulary(models.Model):
     description = models.TextField()
-    intentions = models.ManyToManyField('intention.Intention', related_name='dialogs')
+    intentions = models.ManyToManyField('intention.Intention', related_name='dialogs', blank=True)
     type = models.IntegerField(choices=C_TYPE_VOCABULARY, null=True, blank=True)
     active = models.BooleanField(default=True, null=True, blank=True)
 
@@ -13,3 +13,6 @@ class Vocabulary(models.Model):
         db_table = 'vocabulary'
         verbose_name = 'Vocabulary'
         verbose_name_plural = 'Vocabularies'
+
+    def __str__(self):
+        return f'{self.get_type_display()}'
