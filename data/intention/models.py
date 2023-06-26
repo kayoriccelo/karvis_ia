d3d6a3aa984f28e5d5fdcabf5d3ae30c4bdfb812
Kayo Riccelo <kayo.riccelo@hotmail.com>
2023-06-25 23:11:35 -0300

    - improved app intention (melhorado aplicativo intenção).

--- data/intention/models.py
+++ data/intention/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
+from data.intention.choices import C_TYPE_INTENTION
+
 
 class Intention(models.Model):
     description = models.TextField()
     active = models.BooleanField(default=True, null=True, blank=True)
+    type = models.IntegerField(choices=C_TYPE_INTENTION, null=True, blank=False)
 
     class Meta:
         db_table = 'intention'
