781900ba0d2faefda441958045dabd908f587c5f
Kayo Riccelo <kayo.riccelo@hotmail.com>
2023-07-06 22:07:43 -0300

    - adapted, parameter, rate used in frame_rate to AudioSegment. added new predefined say in speak.

--- speak/background.py
+++ speak/background.py
@@ -1,13 +1,28 @@
 import os
 
+from data.vocabulary.choices import (
+    TYPE_VOCABULARY_OFFER_HELP, TYPE_VOCABULARY_DID_NOT_UNDERSTAND_WHAT_YOU_SAID,
+    TYPE_VOCABULARY_I_DONT_UNDERSTAND_TALKING_AGAIN, TYPE_VOCABULARY_ANYTHING_ELSE,
+    TYPE_VOCABULARY_WHAT_INFORMATION_WANT, TYPE_VOCABULARY_I_STILL_DONT_UNDERSTAND_REPEAT,
+    TYPE_VOCABULARY_WANT_ADD_NEW_INTENT, TYPE_VOCABULARY_WHAT_INTENT_YOU_WANT_CREATE,
+    TYPE_VOCABULARY_WANT_YOU_WANT_SEARCH_CHATGPT, TYPE_VOCABULARY_HOW_MANY_MINUTES_INFORM_KNOWLEDGE,
+    TYPE_VOCABULARY_WHAT_INFORM_KNOWLEDGE, TYPE_VOCABULARY_WHAT_KNOWLEDGE_WANT_TEACH,
+    TYPE_VOCABULARY_THANKS_FOR_THE_NEW_KNOWLEDGE, TYPE_VOCABULARY_I_FOUND_THE_FOLLOWING_INFORMATION,
+    TYPE_VOCABULARY_CONFIRMS_THIS_INFORMATION_FOR_KNOWLEDGE
+)
+from data.vocabulary.methods import try_get_vocabulary
 from useful.exceptions import SpeakSayError
 
 
 class SpeakBackground:
     language = None
+    frame_rate = 26500
 
-    def __init__(self, language):
+    def __init__(self, language, frame_rate=None):
         self.language = language
+
+        if frame_rate:
+            self.frame_rate = frame_rate
 
     def _creating_file_audio_speak(self, speak_text, file_name='speak_audio_ai', file_format='mp3'):
         """
@@ -28,9 +43,8 @@ class SpeakBackground:
 
         return path_name, f'{file_name}.{file_format}'
 
-    @staticmethod
     def _applicate_frame_rate_audio(
-        path_name_origin, file_name_format_origin, file_name='speak_audio_from_rate_ai', file_format='mp3'
+        self, path_name_origin, file_name_format_origin, file_name='speak_audio_from_rate_ai', file_format='mp3'
     ):
         """
         Used to apply frame rate to audio. to work it is necessary to install 'apt install -y ffmpeg'.
@@ -47,7 +61,7 @@ class SpeakBackground:
         path_file_name_format = f'{path_name_origin}/{file_name}.{file_format}'
 
         audio = AudioSegment.from_file(f'{path_name_origin}/{file_name_format_origin}')
-        audio.frame_rate = 26500  # TODO - Kayo: configuration fram_rate in AI.
+        audio.frame_rate = self.frame_rate
         audio.export(path_file_name_format, format=file_format)
 
         os.remove(f'{path_name_origin}/{file_name_format_origin}')
@@ -78,3 +92,55 @@ class SpeakBackground:
 
         except Exception as e:
             SpeakSayError(f"I can not say. {str(e)}")
+
+    def _say_vocabulary(self, type_vocabulary):
+        vocabulary = try_get_vocabulary(type_vocabulary)  # TODO - Kayo: get vocabulary from learn.
+
+        self.say(vocabulary.description)
+
+        return vocabulary
+
+    def say_offer_help(self):
+        return self._say_vocabulary(TYPE_VOCABULARY_OFFER_HELP)
+
+    def say_did_not_understand_what_you_said(self):
+        return self._say_vocabulary(TYPE_VOCABULARY_DID_NOT_UNDERSTAND_WHAT_YOU_SAID)
+
+    def say_i_dont_understand_talking_again(self):
+        return self._say_vocabulary(TYPE_VOCABULARY_I_DONT_UNDERSTAND_TALKING_AGAIN)
+
+    def say_i_still_dont_understand_repeat(self):
+        return self._say_vocabulary(TYPE_VOCABULARY_I_STILL_DONT_UNDERSTAND_REPEAT)
+
+    def say_anything_else(self):
+        return self._say_vocabulary(TYPE_VOCABULARY_ANYTHING_ELSE)
+
+    def say_what_information_want(self):
+        return self._say_vocabulary(TYPE_VOCABULARY_WHAT_INFORMATION_WANT)
+
+    def say_vocabulary_what_inform_knowledge(self):
+        return self._say_vocabulary(TYPE_VOCABULARY_WHAT_INFORM_KNOWLEDGE)
+
+    def say_what_intent_yout_want_create(self):
+        return self._say_vocabulary(TYPE_VOCABULARY_WHAT_INTENT_YOU_WANT_CREATE)
+
+    def say_want_add_new_intent(self):
+        return self._say_vocabulary(TYPE_VOCABULARY_WANT_ADD_NEW_INTENT)
+
+    def say_how_many_minutes_inform_knowledge(self):
+        return self._say_vocabulary(TYPE_VOCABULARY_HOW_MANY_MINUTES_INFORM_KNOWLEDGE)
+
+    def say_what_you_want_search_chatgpt(self):
+        return self._say_vocabulary(TYPE_VOCABULARY_WANT_YOU_WANT_SEARCH_CHATGPT)
+
+    def say_what_knowledge_want_teach(self):
+        return self._say_vocabulary(TYPE_VOCABULARY_WHAT_KNOWLEDGE_WANT_TEACH)
+
+    def say_thanks_for_the_new_knowledge(self):
+        return self._say_vocabulary(TYPE_VOCABULARY_THANKS_FOR_THE_NEW_KNOWLEDGE)
+
+    def say_i_found_the_following_information(self):
+        return self._say_vocabulary(TYPE_VOCABULARY_I_FOUND_THE_FOLLOWING_INFORMATION)
+
+    def say_confirms_this_information_for_knowledge(self):
+        return self._say_vocabulary(TYPE_VOCABULARY_CONFIRMS_THIS_INFORMATION_FOR_KNOWLEDGE)
